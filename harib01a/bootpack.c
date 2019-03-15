532c56cdcd8c0522d420294c6142167e4c3a59a8
aso <titechei@gmail.com>
2019-03-16 00:13:52 +0900

    day4 e

--- harib01a/bootpack.c
+++ harib01a/bootpack.c
@@ -4,9 +4,20 @@ void write_mem8(int addr, int data);
 void HariMain(void)
 {
 	int i; /* 変数宣言。iという変数は、32ビットの整数型 */
+	char *p;
 
-	for (i = 0xa0000; i <= 0xaffff; i++) {
-		write_mem8(i, 15); /* MOV BYTE [i],15 */
+
+	// for (i = 0xa0000; i <= 0xaffff; i++) {
+	// 	 p = (char *)i; /* c */
+	// 	 *p = i & 0xf;
+	//
+	// 	// write_mem8(i, i & 0xf); /* b */
+	// }
+
+	p = 0xa0000;
+	for (i = 0; i <= 0xffff; i++) {
+		p[i] = i & 0xf; /* e */
+		// *(p + i) = i & 0xf; /* d */
 	}
 
 	for (;;) {
